b612a87911b8270a5c27c058acbdc5648e53fe52
Sidak Singh Aulakh <sidakaulakh@gmail.com>
2020-04-04 22:07:45 +0530

    Initial Commit

---
+++ FantasticView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FantasticView
+//
+//  Created by Sidak Singh Aulakh on 04/04/20.
+//  Copyright © 2020 Sidak Singh Aulakh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
